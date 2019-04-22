e2cc71860d13d46f648d973f8da29983a51b7f9a
AlexGA93 <32788331+AlexGA93@users.noreply.github.com>
2019-04-22 10:25:45 +0200

    adding to hash key derivation

--- hash/hash.py
+++ hash/hash.py
@@ -1,13 +1,22 @@
 import hashlib
+import binascii
+
 
 def hashing_methods(password_hash):
-    #Hashing methods
+    # Hashing methods
     hash_md5 = hashlib.md5(password_hash.encode())
     hash_sha1 = hashlib.sha1(password_hash.encode())
     hash_sha224 = hashlib.sha224(password_hash.encode())
     hash_sha256 = hashlib.sha256(password_hash.encode())
     hash_sha384 = hashlib.sha384(password_hash.encode())
     hash_sha512 = hashlib.sha512(password_hash.encode())
+
+    iterations = 100000
+    kd = hashlib.pbkdf2_hmac('sha256', b'password', b'salt', iterations)
+    key_deri = binascii.hexlify(kd)
+
+    # key_derivation = str(key_deri)
+    # print(key_derivation)
 
     md5_digest = hash_md5.hexdigest()
     sha1_digest = hash_sha1.hexdigest()
@@ -16,28 +25,28 @@ def hashing_methods(password_hash):
     sha384_digest = hash_sha384.hexdigest()
     sha512_digest = hash_sha512.hexdigest()
 
-    #Type = tuple
+    # Type = tuple
     output = (" Input:  ", password_hash,
-              "\n Output: \nmd5: ",md5_digest,
-              "\n sha1: ",sha1_digest,
-              "\n sha224: ",sha224_digest,
-              "\n sha256: ",sha256_digest,
-              "\n sha384: ",sha384_digest, 
-              "\n sha512: ",sha512_digest)
+              "\n Output: \n md5: ", md5_digest,
+              "\n sha1: ", sha1_digest,
+              "\n sha224: ", sha224_digest,
+              "\n sha256: ", sha256_digest,
+              "\n sha384: ", sha384_digest,
+              "\n sha512: ", sha512_digest)
 
-    #convert tuple to string
+    # convert tuple to string
     str = ''.join(output)
-    
-    #Add method to create a file as backup to save info
+    # convert key derivation into string
+    k_d = key_deri.decode("utf-8")
 
-    print(str)
-    return str
+    result = str+"\n key derivation: "+k_d
 
-
+    print(result)
+    return result
 
 
 def main():
-    password_hash = input('Password: ')#GUI Input
+    password_hash = input('Password: ')  # GUI Input
     hashing_methods(password_hash)
 
 
