3562d8b6753ce2a8f2323cc1c25ba77599d9a539
AlexGA93 <32788331+AlexGA93@users.noreply.github.com>
2019-04-23 21:47:17 +0200

    Implementing personal algorithm(inverted bin)

---
+++ Blake/blake.py
@@ -0,0 +1,44 @@
+'''
+Optimized for 64-bit platforms and produces digests of any size between 1 and 64 bytes.
+
+hashlib.blake2b(data=b'', *, digest_size=64, key=b'', salt=b'', person=b'', fanout=1, depth=1, leaf_size=0, node_offset=0, node_depth=0, inner_size=0, last_node=False)
+
+These functions return the corresponding hash objects for calculating BLAKE2b or BLAKE2s. They optionally take these general parameters:
+
+data: initial chunk of data to hash, which must be bytes-like object. It can be passed only as positional argument.
+digest_size: size of output digest in bytes.
+key: key for keyed hashing (up to 64 bytes for BLAKE2b, up to 32 bytes for BLAKE2s).
+salt: salt for randomized hashing (up to 16 bytes for BLAKE2b, up to 8 bytes for BLAKE2s).
+person: personalization string (up to 16 bytes for BLAKE2b, up to 8 bytes for BLAKE2s).
+
+'''
+
+from hashlib import blake2b, blake2s
+
+
+def encrypt_blake2b(message, size):
+
+    blake2b_variable = blake2b(digest_size=size)
+    blake2b_variable.update(message.encode())
+    encrypted_2b = blake2b_variable.hexdigest()
+
+    print(encrypted_2b)
+    return encrypted_2b
+
+
+def encrypt_blake2s(message, size):
+    blake2s_variable = blake2s(digest_size=size)
+    blake2s_variable.update(message.encode())
+    encrypted_2s = blake2s_variable.hexdigest()
+
+    print(encrypted_2s)
+    return encrypted_2s
+
+
+if __name__ == "__main__":
+
+    string = 'Prueba'
+    size = 20
+
+    blake_2b = encrypt_blake2b(string, size)
+    blake_2s = encrypt_blake2s(string, size)
---
+++ algoritmo personal/personal.py
@@ -0,0 +1,35 @@
+
+
+def toBinary(mensaje):
+    # conversion to str
+    mensaje_binario = ' '.join(format(ord(x), 'b') for x in mensaje)
+
+    # conversion to list
+    bynary_array = mensaje_binario.split()
+
+    for i, element in enumerate(bynary_array):
+        if element == '100000':
+            bynary_array[i] = ' '
+
+    # print(bynary_array)  # list
+
+    # converting list to string
+    string_array = ''.join(map(str, bynary_array))
+    print(string_array)
+
+    string_inverted = ''
+    for x in string_array:
+        if x == '1':
+            string_inverted += '0'
+        elif x == '0':
+            string_inverted += '1'
+        elif x == ' ':
+            string_inverted += ' '
+
+    # string_inverted
+    return string_inverted
+
+
+if __name__ == "__main__":
+    mensaje = 'Hello World'
+    print(toBinary(mensaje))
